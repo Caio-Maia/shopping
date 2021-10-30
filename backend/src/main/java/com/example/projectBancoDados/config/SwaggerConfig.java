30365c3bef31d3a8b487cef2aaebe2cf9f5ebb84
Caio Maia <caiojdlmaia@gmail.com>
2021-10-29 23:34:50 -0300

    Adicionando Swagger ao projeto

---
+++ backend/src/main/java/com/example/projectBancoDados/config/SwaggerConfig.java
@@ -0,0 +1,72 @@
+package com.example.projectBancoDados.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket lojaApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.projectBancoDados.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfo(
+                "Meus Controlers",
+                "Api da loja",
+                "1.0",
+                "Terms of Service",
+                new Contact("Caio Maia", "https://github.com/Caio-Maia","caiojdlmaia@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
