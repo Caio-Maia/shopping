dc5f4f21efad2db1e6e3da44df872ed81305aa8d
Caio Maia <caiojdlmaia@gmail.com>
2021-11-13 17:51:53 -0300

    adicionando pattern para nome do vendedor

--- backend/src/main/java/com/example/projectBancoDados/dto/vendedor/VendedorRequest.java
+++ backend/src/main/java/com/example/projectBancoDados/dto/vendedor/VendedorRequest.java
@@ -5,10 +5,12 @@ import com.example.projectBancoDados.entities.enums.Cargo;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 public class VendedorRequest {
 
     @NotBlank(message = "Nome is required.")
+    @Pattern(regexp = "^([a-z][a-z\\s]*[a-z])$", message = "O nome não pode conter números ou catacteres especiais.")
     private String nome;
     @NotNull(message = "Matricula is required.")
     private Long matricula;
